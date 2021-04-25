164df222ec5a4ee0a2d8c3b20f06fb78bfc1d372
zhengjiapeng <zhengjiapeng@coocaa.com>
2021-04-25 14:58:52 +0800

    build(eslint): 添加eslint校验规则

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"></HelloWorld>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import upload from './components/upload.vue'
-import {ref, reactive} from 'vue'
+// import upload from './components/upload.vue'
+// import { ref, reactive } from 'vue'
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
-    upload
+    HelloWorld
+    // upload
   }
 })
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'
 // import/export 只能在模块顶层使用，不能在函数、判断语句等代码块之中引用；require/exports 可以。
 const routes = [
-    {
-        path: '/',
-        name: 'Index',
-        component: () => import('../views/Index.vue')
-    },
-    {
-        path: '/table',
-        name: 'Table',
-        component: () => import('../views/Table.vue')
-    }
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('../views/Index.vue')
+  },
+  {
+    path: '/table',
+    name: 'Table',
+    component: () => import('../views/Table.vue')
+  }
 
-];
+]
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
+const router = createRouter({ history: createWebHashHistory(), routes })
 
 // router.beforeEach((to, from, next) => {
 //    if(to.path === '/') {
@@ -27,4 +24,4 @@ const router = createRouter({
 //    }
 // })
 
-export default router;
+export default router
